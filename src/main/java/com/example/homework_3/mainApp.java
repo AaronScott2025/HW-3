47fcf08467ac5fd51ceb4f674f8562b0c638dc37
AaronScott2025 <147019715+AaronScott2025@users.noreply.github.com>
2024-09-16 11:45:59 -0400

    Maze Loads

--- src/main/java/com/example/homework_3/mainApp.java
+++ src/main/java/com/example/homework_3/mainApp.java
@@ -14,16 +14,26 @@ public class mainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         //Mazes Loaded
-        Image maze1 = new Image("GUIElements/maze.png"); //609 X 430
+        Image maze1 = new Image(getClass().getResourceAsStream("/assets/GUI/maze.png")); //609 X 430
         ImageView view1 = new ImageView(maze1); view1.setFitWidth(609);view1.setFitHeight(430);
-        Image maze2 = new Image("GUIElements/maze2.png"); //609 X 430
+        Image maze2 = new Image(getClass().getResourceAsStream("/assets/GUI/maze2.png")); //609 X 430
         ImageView view2 = new ImageView(maze2);view2.setFitWidth(609);view2.setFitHeight(430);
-        //Icon Loaded
-        Image car = new Image("GUIElements/car.png");
-        Image robot = new Image("GUIElements/robot.png");
+        //Icons Loaded
+
+        Image car = new Image(getClass().getResourceAsStream("/assets/GUI/car.png"));
+        Image robot = new Image(getClass().getResourceAsStream("/assets/GUI/robot.png"));
         //Create and Initialize TabPane
+
         TabPane tb = new TabPane();
-        Tab tb1 = new Tab(); tb1.setText("Maze1");
+        Tab tb1 = new Tab(); tb1.setText("Maze1"); tb1.setContent(view1);
+        Tab tb2 = new Tab(); tb2.setText("Maze2"); tb2.setContent(view2);
+        tb.getTabs().add(tb1);tb.getTabs().add(tb2);
+        //Innit and show the scene
+
+        Scene scene = new Scene(tb, 619, 455);
+        stage.setScene(scene);
+        stage.setTitle("Maze Project | Group 5");
+        stage.show();
 
     }
 
